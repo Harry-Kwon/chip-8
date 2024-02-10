61f57e85642d2e255b176da9e22b6ed2366be130
Harry Kwon <harrykwon20@gmail.com>
2024-02-10 16:31:07 -0500

    run multiple instructions

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,60 @@
 struct CPU {
-    current_operation: u16,
-    registers: [u8; 2],
+    registers: [u8; 16],
+    program_counter: usize,
+    memory: [u8; 0x1000], // 4096 bytes (4KiB) memory
 }
 
 impl CPU {
+    fn new() -> Self {
+        return CPU {
+            registers: [0; 16],
+            memory: [0; 0x1000],
+            program_counter: 0,
+        };
+    }
+
     fn read_opcode(&self) -> u16 {
-        return self.current_operation;
+        let p = self.program_counter;
+        let op_byte1 = self.memory[p] as u16;
+        let op_byte2 = self.memory[p + 1] as u16;
+
+        return (op_byte1 << 8) | op_byte2;
     }
 
     fn run(&mut self) {
-        let opcode: u16 = self.read_opcode();
+        loop {
+            let opcode: u16 = self.read_opcode();
+            self.program_counter += 2; // instructions are 16 bits
 
-        let c = ((opcode & 0xf000) >> 12) as u8;
-        let x = ((opcode & 0x0f00) >> 8) as u8;
-        let y = ((opcode & 0x00f0) >> 4) as u8;
-        let d = ((opcode & 0x000f) >> 0) as u8;
+            let c = ((opcode & 0xf000) >> 12) as u8;
+            let x = ((opcode & 0x0f00) >> 8) as u8;
+            let y = ((opcode & 0x00f0) >> 4) as u8;
+            let d = ((opcode & 0x000f) >> 0) as u8;
 
-        match (c, x, y, d) {
-            (0x8, _, _, 0x4) => self.add_xy(x, y),
-            _ => todo!("opcode {:04x}", opcode),
+            match (c, x, y, d) {
+                (0, 0, 0, 0) => {
+                    return;
+                }
+                (0x8, _, _, 0x4) => self.add_xy(x, y),
+                _ => todo!("opcode {:04x}", opcode),
+            }
         }
     }
 
     fn add_xy(&mut self, x: u8, y: u8) {
-        self.registers[x as usize] += self.registers[y as usize];
+        let (val, overflow) =
+            self.registers[x as usize].overflowing_add(self.registers[y as usize]);
+        self.registers[x as usize] = val;
+
+        if overflow {
+            self.registers[0xF] = 1;
+        } else {
+            self.registers[0xF] = 0;
+        }
     }
 }
 
-fn main() {
-    let mut cpu = CPU {
-        current_operation: 0,
-        registers: [0; 2],
-    };
-
-    cpu.current_operation = 0x8014;
-    cpu.registers[0] = 5;
-    cpu.registers[1] = 10;
-}
+fn main() {}
 
 #[cfg(test)]
 mod tests {
@@ -44,17 +62,39 @@ mod tests {
 
     #[test]
     fn adder() {
-        let mut cpu = CPU {
-            current_operation: 0,
-            registers: [0; 2],
-        };
+        let mut cpu = CPU::new();
 
-        cpu.current_operation = 0x8014;
         cpu.registers[0] = 5;
         cpu.registers[1] = 10;
+
+        let mem = &mut cpu.memory;
+        mem[0] = 0x80; // 0x8014 = add (4) register 1 to register 0 (uses 2 registers (8))
+        mem[1] = 0x14;
 
         cpu.run();
 
         assert_eq!(cpu.registers[0], 15);
+    }
+
+    #[test]
+    fn multiplier() {
+        let mut cpu = CPU::new();
+
+        cpu.registers[0] = 5;
+        cpu.registers[1] = 10;
+        cpu.registers[2] = 10;
+        cpu.registers[3] = 10;
+
+        let mem = &mut cpu.memory;
+        mem[0] = 0x80; // 0x8014 = add (4) register 1 to register 0 (uses 2 registers (8))
+        mem[1] = 0x14;
+        mem[2] = 0x80; // 0x8014 = add (4) register 2 to register 0 (uses 2 registers (8))
+        mem[3] = 0x24;
+        mem[4] = 0x80; // 0x8014 = add (4) register 3 to register 0 (uses 2 registers (8))
+        mem[5] = 0x34;
+
+        cpu.run();
+
+        assert_eq!(cpu.registers[0], 35);
     }
 }
